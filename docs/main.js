036dd6c77e6357bb58a51ebf6eab88f83759853d
Davidstafe <141676686+Davidstafe@users.noreply.github.com>
2024-11-18 15:55:58 -0300

    responsive

--- docs/main.js
+++ docs/main.js
@@ -1,31 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Obtener el botón del menú móvil (icono de barras)
-    var barsButton = document.querySelector('.bars');
+// Obtener el ícono de las barras (menú hamburguesa)
+var barsIcon = document.querySelector('.bars');
 
-    // Obtener el menú de la pantalla
-    var menuScreen = document.querySelector('.menu-screen');
+// Obtener la lista del menú
+var menu = document.querySelector('.menu-screen');
 
-    // Agregar un evento de clic al ícono de barras
-    barsButton.addEventListener('click', function() {
-        // Alternar la clase 'show-menu' en el menú para mostrar u ocultar
-        menuScreen.classList.toggle('show-menu');
-    });
-
-    // Agregar eventos de clic a los enlaces del menú para hacer scroll suave
-    var menuLinks = document.querySelectorAll('.menu-screen a');
-    menuLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            // Prevenir el comportamiento predeterminado del enlace
-            event.preventDefault();
-
-            // Obtener el ID de la tarjeta a mostrar desde el atributo href del enlace
-            var targetId = link.getAttribute('href').substring(1);
-
-            // Ocultar el menú después de hacer clic
-            menuScreen.classList.remove('show-menu');
-
-            // Desplazarse a la sección correspondiente
-            document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-        });
-    });
+// Agregar el evento de clic al icono de las barras
+barsIcon.addEventListener('click', function() {
+    // Alternar la clase 'show' en el menú, para mostrar/ocultar
+    menu.classList.toggle('show-menu');
 });
